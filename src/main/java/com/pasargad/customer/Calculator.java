6120e41705b44670cb8e5a0b48ff9f90a4a99447
Maryam <alimadadi.mary@yahoo.com>
2026-09-10 13:02:06 +0200

    Set Up Maven Project with JUnit

---
+++ src/main/java/com/pasargad/customer/Calculator.java
@@ -0,0 +1,7 @@
+package com.pasargad.customer;
+
+public class Calculator {
+	 public int add(int a, int b) {
+	        return a + b;
+	    }
+}
---
+++ src/test/java/com/pasargad/customer/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.pasargad.customer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	 void shouldAddTwoNumbers() {
+
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act
+        int result = calculator.add(2, 3);
+
+        // Assert
+        assertEquals(5, result);
+    }
+
+}
